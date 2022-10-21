e9fc9c7b912a8550763bbdf26fe3b5e69a5cb1e1
shreyashdj <shreyashjadhav1799@gmail.com>
2022-10-21 16:32:47 +0530

    [Shreyash]: Added query to create AddressBook table with required attributes

--- AddressBook.sql
+++ AddressBook.sql
@@ -1,2 +1,7 @@
-/* create database AddressBook */
+-- UC-1 : create AddressBook database
 create database AddressBook;
+
+-- UC-2 : create AddressBook table
+create table AddressBook
+(firstName varchar(50), lastName varchar(50), address varchar(50), city varchar(50),
+state varchar(50), zip varchar(50), phoneNumber varchar(50), email varchar(50));
